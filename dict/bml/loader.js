9e4624a9f6243b51d20c4aa229b6542521f3e260
Andy.F <admin@ajaxsys.net>
2014-01-20 11:03:06 +0900

    Fix dict not work in some old site.

--- dict/bml/loader.js
+++ dict/bml/loader.js
@@ -6,8 +6,9 @@ void((function(w){
     D.DEV_MODE=true;
     if (!D.loaded) {
         var ui=document.createElement('script');
-        ui.setAttribute('src',
-            'http://localhost:8443/build/dict_ui.js?_'+new Date().getTime());
+        ui.setAttribute('src','http://localhost:8443/build/dict_ui.js?_'+new Date().getTime());
+        ui.setAttribute("type","text/javascript");
+        ui.setAttribute("charset","UTF-8");
         D.appendTag(ui);
     }
 })(window));
