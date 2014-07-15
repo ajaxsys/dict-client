23bd6739bc507e1e7544aa32dd11b157a0001cb7
Fang Dehui <javaeecoding@gmail.com>
2014-07-16 00:05:54 +0900

    Cleaner user script

--- dict/bml/loader_userscript.js
+++ dict/bml/loader_userscript.js
@@ -1,72 +1,103 @@
-/**
- * From http://userscripts.org/scripts/show/293217:
-
 // ==UserScript==
-// @name        Dict
-// @namespace   dict-admin.appspot.com
-// @description Dict a second screen service
-// @require     https://dict-admin.appspot.com/dict/dict_userscript.js
-// @include     http://*
-// @include     https://*
-// @grant       none
-// @version     1
+// @name       SecondScreen
+// @namespace  https://dict-admin.appspot.com
+// @version    0.4.2
+// @description  DICT： a second screen for your browser.
+// @match      http://*/*
+// @match      https://*/*
+// @copyright  2014+, fcr403@gmail.com
 // ==/UserScript==
 
+/**
+ * For http://userscripts.org/scripts/show/293217
  */
 
 
 // Avoid iframe
-//if(window != window.top) return;
+/*
+if(window != window.top) {
+    return;
+}*/
 
 // Enable iframe only when size > 300x300
 if(isSkipFrame()) return;
 
+function getBrowserSize(){
+    var w = 0, h = 0;
+    try{
+        //IE
+        if(!window.innerWidth){
+            if(document.documentElement.clientWidth !== 0){
+                //strict mode
+                w = document.documentElement.clientWidth;h = document.documentElement.clientHeight;
+            } else{
+                //quirks mode
+                w = document.body.clientWidth;h = document.body.clientHeight;
+            }
+        } else {
+            //w3c
+            w = window.innerWidth;h = window.innerHeight;
+        }
+    }catch(e){
+        w = 0;
+        h = 0;
+    }
+    
+    return {
+        'width':w?w:0 ,
+        'height':h?h:0
+    };
+}
+
 function isSkipFrame(){
     try {
+        var size = getBrowserSize();
         
-        // Top page, enable
-        if (window == window.top)
+        console.log("0----------",window.top, window.location.href, size.width, size.height);
+        // Top page, not skip == enable
+        if (window == window.top){
+            console.log("1----------Top page",window.top, window.location.href, size.width, size.height);
             return false;
+        }
         
-        // Disable on DICT self page
+        // skip DICT self page == disable
         if (window.location.href.indexOf('dict-admin.appspot.com/dict/proxy.html') > 0 ){
+            console.log("2----------self page",window.top, window.location.href, size.width, size.height);
             return true;
         }
 
-        var size = getBrowserSize();
-        //console.log("----------",window.top, window.location.href, size.width, size.height);
-        // DICT needs at least a 300*300 area
-
-        // Enable dict on other host iframe. 
-        if ((window != window.top && window.top && window.top.location) &&   
-            (window.location.host+window.location.port !== window.top.location.host+window.top.location.port) &&
-            (size.height>300 || size.width>300))
-            return false; 
-    }catch(e){
-        // Enable dict on other host iframe. (Only same host iframe can get top location)
-        return false; 
-    }
-    return true;
-}
-
-
-function getBrowserSize(){
-    var w = 0;var h = 0;
-    //IE
-    if(!window.innerWidth){
-        if(document.documentElement.clientWidth !== 0){
-            //strict mode
-            w = document.documentElement.clientWidth;h = document.documentElement.clientHeight;
-        } else{
-            //quirks mode
-            w = document.body.clientWidth;h = document.body.clientHeight;
+        
+        // skip iframe size less than 300 - DICT needs at least a 300*300 area
+        if (size.height < 300 && size.width<300) {
+            console.log("3----------small than 300*300", window.location, window.top.location);
+            return true;
+            /*
+            if ((window != window.top) &&  window.top && window.top.location
+                (window.location.host+window.location.port !== window.top.location.host+window.top.location.port)
+                ){
+                console.log("3---------", window.location, window.top.location);
+                return false; 
+            }*/
         }
-    } else {
-        //w3c
-        w = window.innerWidth;h = window.innerHeight;
+
+    }catch(e){
+        console.log("err---------" + e);
+        // Error, skip it
+        return true; 
     }
-    return {width:w,height:h};
+    console.log("4-----------ok show it.");
+    // not skip
+    return false;
 }
+
+
+
+
+
+
+
+
+
 
 /*
  * Share with bookmarklet. No jQuery
